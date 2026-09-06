3813448efbf1001db922cb37f454374a3e728b98
waroengaisha-maker <waroeng.aisha@gmail.com>
2026-09-06 19:32:57 +0700

    implementasi menu Akun & Langganan

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -122,9 +122,9 @@ const cards = [
     <div class="flex flex-col gap-6">
         <div class="flex flex-wrap items-end justify-between gap-4">
             <div><Tag value="OVERVIEW" severity="secondary" /><h1 class="mt-2 text-3xl font-bold">Dashboard</h1><p class="mt-2 text-color-secondary">Selamat datang<span v-if="page.props.auth?.user?.name">, {{ page.props.auth.user.name }}</span>.</p></div>
-            <Link href="/finance/reconciliation" class="no-underline">
+            <!-- <Link href="/finance/reconciliation" class="no-underline">
                 <Button label="Lihat Detail Rekonsiliasi" icon="pi pi-list-check" outlined />
-            </Link>
+            </Link> -->
         </div>
         <Card>
             <template #content>
